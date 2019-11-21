b1e141c0a8eaedc7292bc3d9bfb1ace6b72d9e09
Your Name <you@example.com>
2019-11-21 09:24:25 +0000

    adding spring files to projectservice

---
+++ src/test/java/io/turntabl/projectservice/ProjectserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.turntabl.projectservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
